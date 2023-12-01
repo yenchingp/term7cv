593bad8d3f9a7e706a482aa7ca37e7dc20a5df1c
zach <zach_lim@mymail.sutd.edu.sg>
2023-12-02 03:07:04 +0800

    Part 2 complete

--- GUI.py
+++ GUI.py
@@ -1,7 +1,9 @@
-import platform
+import os
+import random
 import GUI_utils
+import numpy as np
 import tkinter as tk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps
 from tkinter import ttk, filedialog, messagebox
 from concurrent.futures import ThreadPoolExecutor
 
@@ -20,17 +22,30 @@ class GUI(tk.Frame):
 
         self.input_image_label = None
         self.output_image_label = None
+        self.cluster_image_label = None
+
+        self.extraction_complete = False
+
+        self.detected_objects = tk.StringVar(value='---')
+        self.inventory_count = None
+        self.cluster_tree = None
+        self.selected_cluster = tk.StringVar(value='')
+
+        self.obj_output_path = tk.StringVar(value='')
 
         ######### UI Elements Placement #########
         self.frame_1 = self.create_frame_1(root)
-        self.frame_1.grid(column=0, row=0, padx=20, pady=20, sticky=tk.N)
+        self.frame_1.grid(row=0, column=0, padx=20, pady=10, sticky=tk.N)
+
+        self.frame_2 = self.create_frame_2(root)
+        self.frame_2.grid(row=1, column=0, padx=20, pady=10, sticky=tk.W)
 
 
     ######### UI Element Creation #########
     def create_frame_1(self, container):
         frame_1 = ttk.LabelFrame(container, padding=5, text='Step 1: Object Detection')
 
-        choose_file_button = ttk.Button(frame_1, text='Choose Image...', command=self.choose_image).grid(row=0, column=0, padx=5, sticky=tk.W)
+        choose_file_button = ttk.Button(frame_1, text='Choose Image...', command=self.choose_image).grid(row=0, column=0, sticky=tk.W)
         file_name_label = ttk.Label(frame_1, textvariable=self.image_name).grid(row=0, column=1, padx=5, sticky=tk.W)
         
         input_image_frame = ttk.Frame(frame_1, width=315, height=315, borderwidth=5, relief='groove')
@@ -38,7 +53,7 @@ class GUI(tk.Frame):
         self.input_image_label = ttk.Label(input_image_frame, image=None)
         self.input_image_label.place(relx=.5, rely=.5, anchor='center')
 
-        run_button = ttk.Button(frame_1, text='Run --->', command=self.run_button_click).grid(row=1, column=2, padx=10)
+        run_detection_button = ttk.Button(frame_1, text='Run --->', command=self.detection_button_click).grid(row=1, column=2, padx=10)
 
         choose_output_button = ttk.Button(frame_1, text='Choose Output Folder...', command=self.choose_output).grid(row=0, column=3, sticky=tk.W)
         output_folder_label = ttk.Label(frame_1, textvariable=self.output_folder).grid(row=0, column=4, sticky=tk.W)
@@ -50,11 +65,58 @@ class GUI(tk.Frame):
 
         return frame_1
     
+
     def create_frame_2(self, container):
         frame_2 = ttk.LabelFrame(container, padding=5, text='Step 2: Object Clustering')
 
+        text_frame = ttk.Frame(frame_2)
+        text_frame.grid(row=0, column=0, sticky=tk.NW)
+        ttk.Label(text_frame, text='No. of Objects Detected: ').grid(row=0, column=0, sticky=tk.W)
+        detected_objects_label = ttk.Label(text_frame, textvariable=self.detected_objects, width=3).grid(row=1, column=0)
+
+        run_clustering_button = ttk.Button(frame_2, text='Run Clustering --->', command=self.clustering_button_click).grid(row=1, column=0)
+
+        self.cluster_tree = self.create_cluster_tree(frame_2)
+        self.cluster_tree.grid(row=0, column=1, padx=15, rowspan=4)
+        self.cluster_tree.bind('<ButtonRelease-1>', self.select_item)
+
+        cluster_image_frame = ttk.Frame(frame_2, width=315, height=315, borderwidth=5, relief='groove')
+        cluster_image_frame.grid(row=0, column=2, rowspan=4, pady=5)
+        self.cluster_image_label = ttk.Label(cluster_image_frame, image=None)
+        self.cluster_image_label.place(relx=.5, rely=.5, anchor='center')
+
+        return frame_2
+    
+
+    def create_cluster_tree(self, container):
+        tree_cols = ['CLU', 'NUM_OBJ']
+
+        tree=ttk.Treeview(container, columns=tree_cols, show='headings', height=15)
+        tree.heading('CLU', text='Clusters')
+        tree.heading('NUM_OBJ', text='No. of Objects')
+        for col in tree['columns']:
+            tree.column(col, anchor='center', stretch='no')
+        
+        tree.column('CLU', anchor='center', stretch='no', width=120)
+        tree.column('NUM_OBJ', anchor='center', stretch='no', width=120)
+
+        return tree
 
     ######### Button Helper Functions #########
+    def select_item(self, a):
+        tree_item = self.cluster_tree.focus()
+        self.selected_cluster.set(self.cluster_tree.item(tree_item)['values'][0])
+        
+        thumbnail_name = self.selected_cluster.get().lower().replace(" ", "_")
+        thumbnail_path = f'{self.obj_output_path.get()}/clusters/thumbnails/{thumbnail_name}.jpg'
+
+        img = Image.open(thumbnail_path).resize((300, 300), Image.LANCZOS)
+        img = ImageTk.PhotoImage(img)
+
+        self.cluster_image_label.configure(image=img)
+        self.cluster_image_label.image=img
+
+
     def choose_image(self):
         img_path = filedialog.askopenfilename(title='Choose Image to Process', filetypes=[("Image Files", ".jpg .png")], initialdir='./GUI_image_test')
         
@@ -79,7 +141,7 @@ class GUI(tk.Frame):
             self.output_path.set(output_path)
 
 
-    def run_button_click(self):
+    def detection_button_click(self):
         if self.image_path.get() == '':
             messagebox.showerror(title='Error', message='No Image Selected!')
             return
@@ -96,12 +158,16 @@ class GUI(tk.Frame):
         
         if extract_status.result():
             print('Extraction Complete!')
+            self.extraction_complete = True
             thread_pool.shutdown()
 
-        self.display_output()
+        self.display_annotated_img()
+
+        self.obj_output_path.set(f'{self.output_path.get()}/{self.image_name.get().split(".")[0]}_objects')
+        self.detected_objects.set(str(len(os.listdir(self.obj_output_path.get()))))
 
 
-    def display_output(self):
+    def display_annotated_img(self):
         annotated_img_path = f'{self.output_path.get()}/{self.image_name.get().split(".")[0]}_annotated.jpg'
 
         img = Image.open(annotated_img_path).resize((300, 300), Image.LANCZOS)
@@ -111,6 +177,81 @@ class GUI(tk.Frame):
         self.output_image_label.image=img
 
 
+    def clustering_button_click(self):
+        if not self.extraction_complete:
+            messagebox.showerror(title='Error', message='Run Object Detection first!')
+            return
+
+        thread_pool = ThreadPoolExecutor(1)
+
+        extract_features = thread_pool.submit(GUI_utils.extract_vgg19_features, self.obj_output_path.get())
+        features_dict = extract_features.result()
+
+        print('Features Extracted!')
+
+        reduce_dims = thread_pool.submit(GUI_utils.dim_reduction_umap, features_dict)
+        reduced_features = reduce_dims.result()
+
+        print('Dimensionality Reduction Completed!')
+
+        cluster_objs = thread_pool.submit(GUI_utils.clustering_AP, reduced_features, self.obj_output_path.get())
+        self.inventory_count = cluster_objs.result()
+
+        print('Objects Clustered!')
+        
+        self.generate_collages(f'{self.obj_output_path.get()}/clusters')
+        
+        print('Thumbnails Generated!')
+
+        self.populate_cluster_tree(self.cluster_tree, self.inventory_count)
+    
+
+    def populate_cluster_tree(self, tree: ttk.Treeview, inv_count: dict):
+        tree.delete(*tree.get_children())
+
+        for (cluster, num) in sorted(inv_count.items()):
+            if cluster == -1:
+                tree.insert('', tk.END, values=[f'Unclustered (Noise)', num])
+            else:    
+                tree.insert('', tk.END, values=[f'Cluster {cluster}', num])
+    
+
+    def generate_collages(self, cluster_root_path):
+        os.makedirs(f'{cluster_root_path}/thumbnails', exist_ok=True)
+        
+        for cluster_path in os.listdir(cluster_root_path):
+            if cluster_path != 'thumbnails':
+                image_paths = [os.path.join(f'{cluster_root_path}/{cluster_path}', f) 
+                            for f in os.listdir(f'{cluster_root_path}/{cluster_path}') if f.endswith('.jpg')]
+                
+                num_images = len(image_paths)
+                grid_size = min(int(np.sqrt(num_images)), 4)
+
+                image_array = random.choices(image_paths, k=np.square(grid_size))
+                image = self.concat_images(image_array, (300, 300), (grid_size, grid_size))
+                image.save(f'{cluster_root_path}/thumbnails/{cluster_path}.jpg', 'JPEG')
+
+
+    def concat_images(self, image_paths, size, shape=None):
+        # Open images and resize them
+        width, height = size
+        images = map(Image.open, image_paths)
+        images = [ImageOps.fit(image, size, Image.LANCZOS) 
+                for image in images]
+        
+        # Create canvas for the final image with total size
+        shape = shape if shape else (1, len(images))
+        image_size = (width * shape[1], height * shape[0])
+        image = Image.new('RGB', image_size)
+        
+        # Paste images into final image
+        for row in range(shape[0]):
+            for col in range(shape[1]):
+                offset = width * col, height * row
+                idx = row * shape[1] + col
+                image.paste(images[idx], offset)
+        
+        return image
             
 
 ########## Main Function ##########
--- GUI_utils.py
+++ GUI_utils.py
@@ -1,3 +1,7 @@
+########################################
+######      UTILS FOR PART 1      ######
+########################################
+
 import os
 import csv
 from ultralytics import YOLO
@@ -47,3 +51,97 @@ def extract_objects(img_path: str, img_name: str, output_path: str):
 
     return True
 
+
+
+########################################
+######      UTILS FOR PART 2      ######
+########################################
+
+import umap
+import shutil
+import numpy as np
+from sklearn import metrics
+from sklearn.cluster import AffinityPropagation
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+
+from tensorflow.keras.applications import vgg19
+
+
+def extract_vgg19_features(img_dir):
+    # Load Model
+    model = vgg19.VGG19(weights='imagenet', include_top=False)
+
+    # Feature Extraction
+    features_dict = {}
+    for img_name in os.listdir(img_dir):
+        image_path = os.path.join(img_dir, img_name)
+        if image_path.lower().endswith(('.png', '.jpg', '.jpeg')):  # Check to ensure only images are processed
+
+
+            # Load and preprocess the image
+            img = image.load_img(image_path, target_size=(224, 224))
+            img = image.img_to_array(img)
+            img = vgg19.preprocess_input(img)
+            img = np.expand_dims(img, axis=0)
+
+            # Extract features from the desired layer (e.g., block5_pool)
+            layer_name = 'block5_pool'  # You can choose a different layer
+            intermediate_layer_model = tf.keras.models.Model(inputs=model.input, outputs=model.get_layer(layer_name).output)
+            features_dict[img_name] = intermediate_layer_model.predict(img)
+
+    return features_dict
+
+
+def dim_reduction_umap(features_dict):
+    feature_list = [features for features in features_dict.values()]
+    feature_matrix = np.array(feature_list).reshape(len(feature_list), -1) # Reshape to (n_samples, n_features) if necessary
+
+    # Apply UMAP
+    reducer = umap.UMAP(n_neighbors=15, n_components=2, metric='euclidean', random_state=42)
+    embedding = reducer.fit_transform(feature_matrix)
+
+    reduced_features = {name: emb for name, emb in zip(features_dict.keys(), embedding)}
+
+    return reduced_features
+
+
+def clustering_AP(reduced_features, img_dir):
+    # 'reduced_features' to list of feature coordinates for clustering
+    X = np.array(list(reduced_features.values()))
+
+    af = AffinityPropagation().fit(X)
+    af_labels = af.labels_
+
+    cluster_dir_base = os.path.join(img_dir, 'clusters')
+    os.makedirs(cluster_dir_base, exist_ok=True)
+
+    for filename in os.listdir(cluster_dir_base):
+        file_path = os.path.join(cluster_dir_base, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    inventory_count = {}
+
+    for img_name, cluster_label in zip(reduced_features.keys(), af_labels):
+        if cluster_label == -1:
+            # -1 for noise points
+            cluster_path = os.path.join(cluster_dir_base, 'noise')
+        else:
+            cluster_path = os.path.join(cluster_dir_base, f'cluster_{cluster_label}')
+        
+        os.makedirs(cluster_path, exist_ok=True)
+        
+        source = os.path.join(img_dir, img_name)
+        destination = os.path.join(cluster_path, img_name)
+        shutil.copy(source, destination)
+        
+        inventory_count[cluster_label] = inventory_count.get(cluster_label, 0) + 1
+
+    return inventory_count
